d03ec8c3df2ebdf59157ada3960a221c3a353906
Juampa <jbevegni@fi.uba.ar>
2024-11-24 18:20:47 -0300

    Creado el setup de la pagina para trabajar con el backend

---
+++ pokemon_svelte/src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Bienvenido a la pokedex de "Duo.py"</h1>
+<p>Acá vas a encontrar la info de todos tus pokemones favoritos, ponete cómodo</p>
---
+++ pokemon_svelte/src/routes/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+<nav>
+   <ul>
+       <li>
+           <a href="/">Home</a>
+       </li>
+       <li>
+           <a href="/pokemons">Pokemones</a>
+       </li>
+       <li>
+           <a href="/teams">Equipos</a>
+       </li>
+       <li>
+           <a href="/movimientos">Movimientos</a> 
+       </li>
+   </ul>
+</nav>
+</header>
