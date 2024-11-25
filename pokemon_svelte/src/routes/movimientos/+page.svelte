213bf936cc50036511a83c13a56691af7b599a0e
Federico <fgianella@fi.uba.ar>
2024-11-25 18:57:53 -0300

    Creo pagina de movimientos donde se muestran los mismos y creo busqueda de movimientos por ID, también mejoro el estilo de la pagina

--- pokemon_svelte/src/routes/movimientos/+page.svelte
+++ pokemon_svelte/src/routes/movimientos/+page.svelte
@@ -1,1 +1,79 @@
-<h1>Esta es la pagina de los movimientos</h1>
+<script>
+    export let data;
+</script>
+
+<h1 class="titulo">MOVIMIENTOS</h1>
+
+
+<style>
+    :global(body) {
+        background: #f1e96f; 
+    }
+    .titulo { 
+        text-align: auto;
+        font-family: sans-serif;
+        color: #4b4ef7;
+        background-color: #dfdec3; 
+        border: 3px solid #000; 
+        padding: 10px 10px;
+        box-shadow: 2px 2px 8px rgba(0, 0, 5, 3);
+         
+    }
+
+    table {
+        width: 75%;
+        margin: auto;
+        border-collapse: collapse;
+        background-color: #dfdec3;
+        border: 6px solid black
+    }
+
+    th, td {
+        text-align: center;
+        padding: 30px;
+        border: 1px solid black
+    }
+
+    th {
+        background-color: #d83535; 
+        color: #ffffff;
+        font-size: 18px;
+    }
+
+    td {
+        font-size: 20px;
+    }
+
+    a {
+        color: #1a73e8;
+    }
+
+
+    h1 {
+        text-align: center;
+        color: #4b4ef7;
+    }
+</style>
+
+<table>
+    <thead>
+        <tr>
+             <th>ID</th>
+             <th>NOMBRE</th>
+             <th>INFORMACIÓN</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each data.moves as moves}
+            <tr>
+                <td>{moves.id}</td>
+                <td>{moves.nombre}</td> 
+                <td>
+                    <a href={`/movimientos/${moves.id}`}>
+                        Ver más información
+                    </a>
+                </td>              
+            </tr>
+        {/each}
+    </tbody>
+ </table>
---
+++ pokemon_svelte/src/routes/movimientos/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let data;
+</script>
+
+<h1>Detalle del movimiento</h1>
+
+<p><strong>ID:</strong> {data.move.id}</p>
+<p><strong>NOMBRE:</strong> {data.move.nombre}</p>
+<p><strong>GENERACIÓN:</strong> {data.move.generacion}</p>
+<p><strong>USOS:</strong> {data.move.usos}</p>
+<p><strong>PRECISIÓN:</strong> {data.move.precision}</p>
+<p><strong>POTENCIA:</strong> {data.move.potencia}</p>
+<p><strong>EFECTOS:</strong> {data.move.efecto}</p>
+
+<a href="/movimientos">Volver a la lista de movs</a>
