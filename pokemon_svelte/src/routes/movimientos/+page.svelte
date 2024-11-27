44a0bb0b1db07f476610e3636e11f1007792e24a
Federico <fgianella@fi.uba.ar>
2024-11-27 16:19:20 -0300

    Añado buscador en la sección de movimientos y de pokemones

--- pokemon_svelte/src/routes/movimientos/+page.svelte
+++ pokemon_svelte/src/routes/movimientos/+page.svelte
@@ -1,6 +1,9 @@
 <script>
+    import Typeahead from "svelte-typeahead";
     export let data;
-    import "../../styles/tablas.css"
+    import "../../styles/tablas.css";
+
+    let Movimientos = data.moves; 
 </script>
 
 <h1 class="titulo">MOVIMIENTOS</h1>
@@ -8,6 +11,23 @@
 <p class="subtitulo">
     Acá podrás ver todos los movimientos que pueden tener tus pokemons favoritos
 </p>
+
+
+<div class="buscador">
+    <Typeahead
+        label="Buscar movimiento"
+        placeholder="Ingresa el nombre del movimiento:"
+        data={data.moves}
+        extract={(moves) => moves.nombre}
+        on:select={({ detail }) => {
+            Movimientos = [detail.original]; 
+        }}
+        inputAfterSelect="clear" 
+    />
+    <button on:click={() => (Movimientos = data.moves)}>
+        Ver todos
+    </button>
+</div>
 
 <table class="tabla_general">
     <thead>
@@ -18,7 +38,7 @@
         </tr>
     </thead>
     <tbody>
-        {#each data.moves as moves}
+        {#each Movimientos as moves}
             <tr>
                 <td>{moves.id}</td>
                 <td>{moves.nombre}</td> 
@@ -31,3 +51,5 @@
         {/each}
     </tbody>
  </table>
+
+
--- pokemon_svelte/src/routes/pokemons/+page.svelte
+++ pokemon_svelte/src/routes/pokemons/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     export let data;
     import '../../styles/tablas.css';
+    import Typeahead from "svelte-typeahead";
+    let filtrados = data.pokemones;
 </script>
 
 <h1 class="titulo">
@@ -9,6 +11,22 @@
 <p class="subtitulo">
     Acá podes ver a todos tus pokemons favoritos
 </p>
+
+<div class="buscador">
+    <Typeahead
+        label="Buscar Pokémon"
+        placeholder="Ingresa el nombre del Pokémon:"
+        data={data.pokemones}
+        extract={(pokemones) => pokemones.nombre}
+        on:select={({ detail }) => {
+            filtrados = [detail.original]; 
+        }}
+        inputAfterSelect="clear" 
+    />
+    <button on:click={() => (filtrados = data.pokemones)}>
+        Ver todos
+    </button>
+</div>
 
 <table class="tabla_general">
    <thead>
@@ -20,7 +38,7 @@
        </tr>
    </thead>
    <tbody>
-       {#each data.pokemones as pokemon}
+       {#each filtrados as pokemon}
            <tr>
                <td>{pokemon.nombre}</td>
                <td><img src={pokemon.imagen} alt={pokemon.nombre} width="100" /></td>
